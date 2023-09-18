3022f2a486c7776c0cb89a9497bbb329220def05
Zach <pittmanzach2@gmail.com>
2023-09-18 17:25:41 -0400

    resolving undefined issue

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,39 +1,61 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userName = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
 
 // hide game screen
-gameScreen.classList.add(`d-none`);
+//gameScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-
+  scoreParagraph.textContent = `${game.username}: ${game.score.user} v CPU: ${game.score.cpu}`;
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
-
+  for (const entry of game.gameHistoryLog) {
+    const historyEntry = document.createElement('p');
+    historyEntry.textContent = entry;
+    gameHistoryParagraph.appendChild(historyEntry);
+  }
 }
+  
+
 
 // start-game-button EventListener
-startGameButton.addEventListener(`click`, function () {
-  const username = 
-  game = new RockPaperScissors(userName);
+
+startGameButton.addEventListener(`click`, function () { 
+  const user_selection = userSelection.value;
+
+  const username = userName.value;
+  game = new RockPaperScissors(username);
+  game.play(user_selection)
+  console.log(game.score.user)
+  // gameScreen.classList.remove(`d-none`);
+  // welcomeScreen.classList.add(`d-none`);
   // Complete
 });
 
-// go-button EventListener
+
+//go-button EventListener
 goButton.addEventListener(`click`, function () {
-  
+  const user_selection = userSelection.value;
+  game.play(user_selection)
+  updateScoreTallyUI(); // Update the score display
+  updateGameHistoryUI(); // Update the game history display
 });
 
 // If you're doing the extra-credit, uncomment the below: reset-game-button
--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -3,7 +3,7 @@ class RockPaperScissors {
     this.username = username;
     this.score = {
       user: 0,
-      cpu:0 
+      cpu:0
     },
     this.gameHistoryLog = [];
   }
@@ -13,9 +13,9 @@ class RockPaperScissors {
    * using Math.random() method, you should be able to get one of the following values
    */
   generateCPUResponse(){
-    const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
+    const acceptedValues = ['rock', 'paper', 'scissors' ];
+    const index = Math.floor(Math.random() * acceptedValues.length);
+    return acceptedValues[index];
   }
   /**
    * returns one of the following values: `win`, `lose`, `tie`
@@ -34,6 +34,38 @@ class RockPaperScissors {
    */
   determineWinner(userSelection, cpuSelection){
 
+    switch(userSelection) {
+      case 'rock':
+       switch(cpuSelection) {
+        case 'rock':
+          return 'tie';
+        case 'paper':
+          return 'lose';
+        case 'scissors':
+          return 'win';
+       } break;
+
+       case 'paper':
+       switch(cpuSelection) {
+        case 'rock':
+          return 'win';
+        case 'paper':
+          return 'tie';
+        case 'scissors':
+          return 'lose';
+       } break;
+
+       case 'scissors':
+       switch(cpuSelection) {
+        case 'rock':
+          return 'lose';
+        case 'paper':
+          return 'win';
+        case 'scissors':
+          return 'tie';
+       } break;
+      
+    }
   }
 
   /**
@@ -41,7 +73,17 @@ class RockPaperScissors {
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
-
+    const cpu_choice = this.generateCPUResponse();
+    const winner = this.determineWinner(userSelection, cpu_choice)
+    console.log(winner)
+    if (winner == 'win') {
+      this.score.user ++;
+    } else if (winner == 'lose') {
+      this.score.cpu ++;
+    } 
+    this.gameHistoryLog.push(
+      `${this.username} selected: ${userSelection}, CPU selected: ${cpu_choice} - ${winner} wins.`
+    );
   }
 
 }
